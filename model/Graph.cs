8deb75563c6867a7a7ff9acd223aa040742f0fe4
OmarKhaled04 <omarkhalid15218@gmail.com>
2025-05-01 05:44:04 +0300

    input readings and Graph data structure is done

--- model/Graph.cs
+++ model/Graph.cs
@@ -1,12 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MAP_routing.model
+﻿namespace MAP_routing.model
 {
+    public class Intersection
+    {
+        public int InterscID;
+        public double Xcoordinate, Ycoordinate;
+
+        public Intersection(int ID, double X, double Y)
+        {
+            InterscID = ID;
+            Xcoordinate = X;
+            Ycoordinate = Y;
+        }
+
+    }
+    public class Road
+    {
+        public int FromInterscID, ToInterscID;
+        public double RoadLength;
+        public List<double> RoadSpeeds;
+        public int SpeedIntrv;
+
+        public Road(int fromInterscID, int toInterscID, double roadLength, List<double> roadSpeeds, int speedIntrv)
+        {
+            FromInterscID = fromInterscID;
+            ToInterscID = toInterscID;
+            RoadLength = roadLength;
+            RoadSpeeds = roadSpeeds;
+            SpeedIntrv = speedIntrv;
+        }
+        public double GetTravelTime(double startTime = 0)
+        {
+            // Normal case : speed is constant 
+
+            int minutesinOnehour = 60;
+            if (SpeedIntrv == 0) return (RoadLength / RoadSpeeds[0]) * minutesinOnehour;
+
+
+            // Bonus case : speed is changing each time interval
+
+            int intervalIndex = (int)(startTime / SpeedIntrv) % RoadSpeeds.Count;
+            double speed = RoadSpeeds[intervalIndex];
+            return (RoadLength / speed) * 60;
+
+        }
+    }
     internal class Graph
     {
+        public Dictionary<int, Intersection> Nodes = new();
+        public Dictionary<int, List<Road>> adjList = new();
+
+        public void AddNode(Intersection node)
+        {
+            Nodes[node.InterscID] = node;
+        }
+        public void AddEdge(Road Edge)
+        {
+            if (!Nodes.ContainsKey(Edge.FromInterscID) || !Nodes.ContainsKey(Edge.ToInterscID))
+                throw new ArgumentException("Invalid intersection ID in road");
+
+
+            if (!adjList.ContainsKey(Edge.FromInterscID))
+                adjList[Edge.FromInterscID] = new List<Road>();
+            if (!adjList.ContainsKey(Edge.ToInterscID))
+                adjList[Edge.ToInterscID] = new List<Road>();
+
+            adjList[Edge.FromInterscID].Add(Edge);
+            adjList[Edge.ToInterscID].Add(new Road(Edge.ToInterscID, Edge.FromInterscID, Edge.RoadLength, new List<double>(Edge.RoadSpeeds), Edge.SpeedIntrv));
+
+        }
+
     }
 }
--- model/inputReading.cs
+++ model/inputReading.cs
@@ -1,12 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MAP_routing.model
+﻿namespace MAP_routing.model
 {
-    internal class Class1
+    internal class inputReading
     {
+        public Graph ParseMapFile(string filePath)
+        {
+            var graph = new Graph();
+            var lines = File.ReadAllLines(filePath);
+            int index = 0;
+            int N = int.Parse(lines[index++]);
+            for (int i = 0; i < N; i++)
+            {
+                var parts = lines[index++].Split();
+                int id = int.Parse(parts[0]);
+                double x = double.Parse(parts[1]), y = double.Parse(parts[2]);
+                Intersection Node = new Intersection(id, x, y);
+                graph.AddNode(Node);
+            }
+            var roadInfo = lines[index++].Split();
+            int M = int.Parse(roadInfo[0]);
+            int speedCount = 1, speedInterval = 0;
+
+
+            if (roadInfo.Length == 3)
+            {
+                speedCount = int.Parse(roadInfo[1]);
+                speedInterval = int.Parse(roadInfo[2]);
+            }
+
+            for (int i = 0; i < M; i++)
+            {
+                var parts = lines[index++].Split();
+                int fromId = int.Parse(parts[0]), toId = int.Parse(parts[1]);
+                double length = double.Parse(parts[2]);
+                var speeds = new List<double>();
+
+
+                for (int j = 3; j < parts.Length; j++)
+                    speeds.Add(double.Parse(parts[j]));
+
+                Road road = new Road(fromId, toId, length, speeds, speedInterval);
+                graph.AddEdge(road);
+            }
+
+            return graph;
+        }
+        public static List<Query> ParseQueryFile(string filePath)
+        {
+            var queries = new List<Query>();
+            var lines = File.ReadAllLines(filePath);
+            int Q = int.Parse(lines[0]);
+
+            for (int i = 1; i <= Q; i++)
+            {
+                var parts = lines[i].Split();
+                queries.Add(new Query
+                {
+                    SourceX = double.Parse(parts[0]),
+                    SourceY = double.Parse(parts[1]),
+                    DestX = double.Parse(parts[2]),
+                    DestY = double.Parse(parts[3]),
+                    MaxWalkDistance = int.Parse(parts[4])
+                });
+            }
+            return queries;
+        }
+        public class Query
+        {
+            public double SourceX { get; set; }
+            public double SourceY { get; set; }
+            public double DestX { get; set; }
+            public double DestY { get; set; }
+            public int MaxWalkDistance { get; set; }
+        }
     }
 }
